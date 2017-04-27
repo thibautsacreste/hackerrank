69208cb0e018478d88d2352f2a11fe1be527c13e
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2017-04-27 14:15:07 +0100

    Using chars seq instead of string vectors

--- challenges/balanced-brackets/boot.clj
+++ challenges/balanced-brackets/boot.clj
@@ -2,21 +2,21 @@
 
 (require '[clojure.string :refer [trim split]])
 
-(def opening ["{" "[" "("])
-(def closing ["}" "]" ")"])
+(def opening (seq "{[("))
+(def closing (seq "}])"))
 (def matching (zipmap opening closing))
 
 (defn -main [& args]
   (let [n (-> (read-line) trim Integer/parseInt)]
     (loop [i n]
       (when (> i 0)
-        (let [brackets (-> (read-line) trim (split #"\s*"))
+        (let [brackets (-> (read-line) trim seq)
               final (reduce
                       (fn [stack bracket]
                         (cond
                           (some #{bracket} opening) (conj stack (get matching bracket))
                           (= (last stack) bracket) (pop stack)
-                          :else (reduced [bracket])))
+                          :else (reduced (conj stack bracket))))
 
                       []
                       brackets)]
