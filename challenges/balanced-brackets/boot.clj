fde71623f298041e057bcca0f79f7520bb87ae9b
Thibaut Sacreste <thibaut.sacreste@gmail.com>
2017-04-27 14:26:44 +0100

    Extracting balanced? fn

--- challenges/balanced-brackets/boot.clj
+++ challenges/balanced-brackets/boot.clj
@@ -6,19 +6,23 @@
 (def closing (seq "}])"))
 (def matching (zipmap opening closing))
 
+(defn balanced? [brackets]
+  (if (> (mod (count brackets) 2) 0)
+    false
+    (->> brackets
+         (reduce
+          (fn [stack bracket]
+            (cond
+              (some #{bracket} opening) (conj stack (get matching bracket))
+              (= (last stack) bracket) (pop stack)
+              :else (reduced (conj stack bracket))))
+          [])
+         empty?)))
+
 (defn -main [& args]
   (let [n (-> (read-line) trim Integer/parseInt)]
     (loop [i n]
       (when (> i 0)
-        (let [brackets (-> (read-line) trim seq)
-              final (reduce
-                      (fn [stack bracket]
-                        (cond
-                          (some #{bracket} opening) (conj stack (get matching bracket))
-                          (= (last stack) bracket) (pop stack)
-                          :else (reduced (conj stack bracket))))
-
-                      []
-                      brackets)]
-          (if (empty? final) (println "YES") (println "NO")))
+        (let [brackets (-> (read-line) trim seq)]
+          (println (if (balanced? brackets) "YES" "NO")))
         (recur (- i 1))))))
